ba0ec9056d360f05ffc5f00d6219f3e289810ad6
Gaudrius <gaudriusk@gmail.com>
2026-05-20 20:53:58 +0300

    prideti operatoriai

--- src/Studentas.cpp
+++ src/Studentas.cpp
@@ -1,8 +1,54 @@
 #include "Studentas.h"
 #include <algorithm>
+#include <iomanip>
 #include <numeric>
+#include <string>
+
 using std::istream;
+using std::left;
+using std::ostream;
+using std::setw;
 using std::sort;
+using std::string;
+using std::vector;
+
+ostream &operator<<(std::ostream &out, const Studentas &s) {
+  out << left << setw(20) << "Vardas" << left << setw(20) << "Pavarde" << left
+      << setw(20) << "Galutinis (Vid.)" << left << setw(20)
+      << "Galutinis (Med.)" << "\n";
+
+  out << string(100, '-') << "\n";
+
+  return out;
+}
+
+istream &operator>>(std::istream &in, Studentas &s) {
+  string vardas, pavarde;
+  in >> vardas >> pavarde;
+  s.setVardas(vardas);
+  s.setPavarde(pavarde);
+
+  int temp;
+  vector<int> pazymiai;
+  while (in >> temp) {
+
+    pazymiai.push_back(temp);
+  }
+  if (!pazymiai.empty()) {
+
+    int egz = pazymiai.back();
+    pazymiai.pop_back();
+    s.setEgzaminoBalas(egz);
+
+    for (int i : pazymiai) {
+      s.addPazymys(i);
+    }
+
+    s.gautiVidurkiVidutini();
+    s.gautiVidurkiMediana();
+  }
+  return in;
+}
 
 void Studentas::surusiuotiPazymius() {
   sort(pazymiai_.begin(), pazymiai_.end());
