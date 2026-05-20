d0e34298b02e7c8e8327e2f26468f9e3bda7d7ed
Gaudrius <gaudriusk@gmail.com>
2026-05-20 18:30:56 +0300

    Prideti konstruktoriai

--- include/Studentas.h
+++ include/Studentas.h
@@ -16,13 +16,59 @@ private:
 public:
   Studentas();
 
+  ~Studentas() {}
+
   Studentas(std::string vardas, std::string pavarde, int namuDarbai,
             std::vector<int> pazymiai, int egzaminoBalas)
       : vardas_(std::move(vardas)), pavarde_(std::move(pavarde)),
         namuDarbai_(namuDarbai), pazymiai_(std::move(pazymiai)),
         egzaminoBalas_(egzaminoBalas) {};
 
-  ~Studentas() {}
+  Studentas(const Studentas &other)
+      : vardas_(other.vardas_), pavarde_(other.pavarde_),
+        namuDarbai_(other.namuDarbai_), pazymiai_(other.pazymiai_),
+        egzaminoBalas_(other.egzaminoBalas_),
+        galutinisVidurkis_(other.galutinisVidurkis_),
+        galutinisMediana_(other.galutinisMediana_) {}
+
+  Studentas &operator=(const Studentas &other) {
+    if (this == &other) {
+      return *this;
+    }
+
+    vardas_ = other.vardas_;
+    pavarde_ = other.pavarde_;
+    namuDarbai_ = other.namuDarbai_;
+    pazymiai_ = other.pazymiai_;
+    egzaminoBalas_ = other.egzaminoBalas_;
+    galutinisVidurkis_ = other.galutinisVidurkis_;
+    galutinisMediana_ = other.galutinisMediana_;
+
+    return *this;
+  }
+
+  Studentas(Studentas &&other)
+      : vardas_(std::move(other.vardas_)), pavarde_(std::move(other.pavarde_)),
+        namuDarbai_(other.namuDarbai_), pazymiai_(std::move(other.pazymiai_)),
+        egzaminoBalas_(other.egzaminoBalas_),
+        galutinisVidurkis_(other.galutinisVidurkis_),
+        galutinisMediana_(other.galutinisMediana_) {}
+
+  Studentas &operator=(Studentas &&other) {
+    if (this == &other) {
+      return *this;
+    }
+
+    vardas_ = std::move(other.vardas_);
+    pavarde_ = std::move(other.pavarde_);
+    namuDarbai_ = other.namuDarbai_;
+    pazymiai_ = std::move(other.pazymiai_);
+    egzaminoBalas_ = other.egzaminoBalas_;
+    galutinisVidurkis_ = other.galutinisVidurkis_;
+    galutinisMediana_ = other.galutinisMediana_;
+
+    return *this;
+  }
 
   std::string getVardas() const { return vardas_; }
   std::string getPavarde() const { return pavarde_; }
