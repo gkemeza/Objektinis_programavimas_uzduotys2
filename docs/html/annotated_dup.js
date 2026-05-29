a5a31b890b0c95e729cde349d216b6ebfa891f17
Gaudrius <gaudriusk@gmail.com>
2026-05-29 11:43:38 +0300

    doxygen

--- docs/html/annotated_dup.js
+++ docs/html/annotated_dup.js
@@ -2,5 +2,6 @@ var annotated_dup =
 [
     [ "Studentas", "classStudentas.html", "classStudentas" ],
     [ "Timer", "classTimer.html", "classTimer" ],
+    [ "Vector", "classVector.html", "classVector" ],
     [ "Zmogus", "classZmogus.html", "classZmogus" ]
 ];
