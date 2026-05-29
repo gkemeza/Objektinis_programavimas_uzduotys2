09c2311b938cd9c434405a546e12cae27bcd58b5
Gaudrius <gaudriusk@gmail.com>
2026-05-29 11:44:11 +0300

    doxygen

--- docs/html/dir_d44c64559bbebec7f509842c48db8b23.js
+++ docs/html/dir_d44c64559bbebec7f509842c48db8b23.js
@@ -3,5 +3,6 @@ var dir_d44c64559bbebec7f509842c48db8b23 =
     [ "functions.h", "functions_8h.html", "functions_8h" ],
     [ "Studentas.h", "Studentas_8h.html", "Studentas_8h" ],
     [ "timer.h", "timer_8h.html", "timer_8h" ],
+    [ "Vector.h", "Vector_8h.html", "Vector_8h" ],
     [ "Zmogus.h", "Zmogus_8h.html", "Zmogus_8h" ]
 ];
